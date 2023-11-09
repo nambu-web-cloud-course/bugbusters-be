8db369b5815ee25006bb09023d4fe959bb88c81d
bomnamu <bomnamu@gmail.com>
2023-11-09 17:31:01 +0900

    add cors

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const dotenv = require('dotenv');
+const cors = require('cors');
 
 dotenv.config();
 const sync = require('./models/sync.js');
@@ -12,6 +13,9 @@ const trade_router = require('./routes/trade_router.js');
 // const isAuth = require('./routes/authorization.js');
 app.use (morgan('dev'));
 app.use (express.json());
+app.use(cors({
+    origin:'*',
+}));
 
 app.get('/', (req,res)=>{
     res.send("hello");
