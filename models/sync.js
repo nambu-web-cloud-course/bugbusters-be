b533c0b9980b0d7ebf17108b0c25eec214265553
bomnamu <bomnamu@gmail.com>
2023-11-21 12:39:47 +0900

    update db

--- models/sync.js
+++ models/sync.js
@@ -2,7 +2,7 @@ const { sequelize } = require('./index.js');
 const sync = ()=> {
 
     sequelize
-        .sync({ force: false})
+        .sync({ force: true})
         .then(() => {
             console.log('데이터베이스 연결됨.');
         })
